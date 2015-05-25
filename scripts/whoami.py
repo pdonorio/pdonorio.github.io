f61f80809ad46aefb89b554b5a6c5d4131327360
paulie <tempo.pause@gmail.com>
2015-05-25 15:27:59 +0200

    Creating an extension to use the whoami module

--- scripts/whoami.py
+++ scripts/whoami.py
@@ -3,22 +3,24 @@
 
 """ Who am i? """
 
+################################
 name = 'Paolo'
 surname = "D'Onorio De Meo"
 work = 'Cineca'
 domain = 'Italy'
+################################
 
 DOT = '.'
 AT = '@'
 SPACE = ' '
 EMPTY = ''
 QUOTE = "'"
-
 mydata = {}
 
-# Argh
+# In case you have a surname with quotes...!
 clean_surname = surname.replace(QUOTE, EMPTY)
 
+#################################################
 # Email address @work
 mydata['email'] = name[0].lower() + DOT + clean_surname.lower().replace(SPACE, EMPTY) + \
     AT + work.lower() + DOT + domain[:2].lower()
@@ -29,7 +31,23 @@ mydata['twitter'] = AT + name.lower() + DOT + \
 mydata['github'] = AT + name[0].lower() + \
     clean_surname[:clean_surname.find(SPACE)].lower()
 
-# Print 'em all
-for account, value in mydata.iteritems():
-    print(account + ': ' + value)
 
+#################################################
+""" Defining a simple ipython extension """
+from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
+
+@magics_class
+class HelloWorldMagics(Magics):
+    """ An extension about my (ego) self """
+    DEFAULT = 42
+
+    @line_magic
+    @cell_magic
+    def helloworld(self, line='', cell=None):
+        print("\n\tWho am i\n")
+        for account, value in mydata.iteritems():
+            print(account + ': ' + value)
+        return self.DEFAULT
+
+def load_ipython_extension(ip):
+    ip.register_magics(HelloWorldMagics)
